e250975a529fc085bc19ea636b2351ed10926e9a
mengyanhong <2297044068@qq.com>
2021-09-10 18:52:02 +0800

    sync_robot配置1.3

--- API_project/Libs/sync_advanced_libs.py
+++ API_project/Libs/sync_advanced_libs.py
@@ -1,43 +1,7 @@
 # @Time : 2021/9/9 18:56
 # @Author : 孟艳红
 # @File : sync_advanced_libs.py 高级搜索转移操作
-# def __init__(self, environment):
-#     self.User = user(environment)
-#
-# def sync(self, pids=None, pages=None, seach_value=None, Quota=True, dataColumns=None, phoneStatus=None,
-#          numberCount=0, needCallPlan=False, way=None):
-#     true = True
-#     false = False
-#     if phoneStatus is None:
-#         phone = [0, 1, 2, 3]
-#     else:
-#         phone = phoneStatus
-#     if dataColumns is None:
-#         dataColumn = [0]
-#     else:
-#         dataColumn = dataColumns
-#     url = f'https://{self.User.robot_Host()}/api_skb/v1/clues/sync_robot'
-#     payload = {
-#         "from": "syncRobot",
-#         "useQuota": Quota,  # 是否使用额度
-#         "dataColumns": dataColumn,  # 数据字段[0, 1] // 0: 手机，1：固话
-#         "phoneStatus": phone,  # 手机过滤 [0, 1, 2 , 3] //[0, 1, 3]: 过滤疑似代理记账号码 [0, 1, 2]: 过滤异常号码
-#         "numberCount": numberCount,  # 号码数量 0: 全部,1: 第一条
-#         "canCover": false,
-#         "needCallPlan": needCallPlan,  # 是否需要创建外呼计划 true / false
-#     }
-#     if way is not None:
-#         payload.update({"way": way})
-#     payload.update(seach_value)
-#     if pids is None:
-#         payload.update({"page": 1, "pagesize": pages})
-#     else:
-#         payload.update({"pids": pids, })
-#         payload.pop('page', 'pagesize')
-#
-#     heade = self.User.shop_headers()
-#     response = requests.post(url=url, headers=heade, json=payload)
-#     return response
+
 import time
 from pprint import pprint
 
@@ -67,7 +31,7 @@ class Sync_robot_test:
                               "syncRobotRangeDate": []}, "scope": "companyname", "matchType": "most_fields",
                    "pagesize": 10, "page": 1}
         header = self.user.shop_headers()
-        response = requests.post(url, headers=header, json=payload, verify=False)
+        response = requests.post(url, headers=header, json=payload, verify=False) #搜索未查看，未转机器人的数据
         request_payload = response.request.body.decode("unicode_escape")
         request_payloa = json.loads(request_payload)
         resp_items = response.json()['data']['items']
@@ -93,7 +57,6 @@ class Sync_robot_test:
                     sync_sum+=1
                     print(i,'\n',resp_robot.json())
                 assert len(resp_robot.json()['data']['list']) ==1
-
             if sync_sum >= len(companyName):
                 print('转移失败')
         else:
