36a6d8c5f7190e9707e375c1b4d696b5adeec20e
mengyanhong <2297044068@qq.com>
2021-09-13 18:02:31 +0800

    sync_robot配置1.7

--- API_project/Test_files/robot_test.py
+++ API_project/Test_files/robot_test.py
@@ -1,43 +1,48 @@
 # @Time : 2021/9/9 18:56
 # @Author : 孟艳红
 
-import time
+import time,pytest,requests, json
 from pprint import pprint
-
-import requests, json
 from API_project.Configs.config_API import user
-# from API_project.Libs.robot_libs import Robotlist
 from API_project.Configs.search_API import search
 from API_project.Libs.sync_robot_libs import Sync_robot
-
 from API_project.Libs.sync_robot_search_libs import Sync_robot_test
 
-test_host = "lxcrm"  # 设置测试环境 test:测试环境，staging:回归环境，lxcrm:正式环境
+test_host = "test"  # 设置测试环境 test:测试环境，staging:回归环境，lxcrm:正式环境
 user = user(test_host)
-# self.Robotlist = Robotlist(test_host)
 Sync_robot = Sync_robot(test_host)
 search = search(test_host)
-
 Sync_robot_test = Sync_robot_test(test_host)
 
 class Test_Sync_robot:
-    def test_case01(self):  # 扣除流量额度，转移手机和固话，仅1条号码,不创建外呼计划
-        Sync_robot_test.case01()
+    # @pytest.mark.parametrize('way', ['search_list', 'advanced_search_list', None])
+    # @pytest.mark.parametrize('page', [None, 500, 1000, 2000])
 
-    def test_case02(self):  # 不扣除流量额度，转移手机和固话，仅1条号码,不创建外呼计划，
-        Sync_robot_test.case02()
+    @pytest.mark.parametrize('way',['search_list'])
+    @pytest.mark.parametrize('page', [None, 500])
+    def test_case01(self,way,page):  # 扣除流量额度，转移手机和固话，仅1条号码,不创建外呼计划
+        Sync_robot_test.case01(way = way,page=page)
 
-    def test_case03(self):  # 不扣除流量额度，转移手机和固话，仅1条号码,不创建外呼计划，
-        Sync_robot_test.case03()
+    @pytest.mark.parametrize('way', ['search_list'])
+    @pytest.mark.parametrize('page', [None, 500])
+    def test_case02(self,way,page):  # 不扣除流量额度，转移手机和固话，仅1条号码,不创建外呼计划，
+        Sync_robot_test.case02(way = way,page=page)
 
-    def test_case04(self):  # 不扣除流量额度，转移手机和固话，仅1条号码,不创建外呼计划，
-        Sync_robot_test.case04()
+    @pytest.mark.parametrize('way', ['search_list'])
+    @pytest.mark.parametrize('page', [None, 500])
+    def test_case03(self,way,page):  # 仅转移已查看数据，不扣除流量额度，转移手机和固话，仅1条号码,不创建外呼计划，
+        Sync_robot_test.case03(way = way,page=page)
+
+    @pytest.mark.parametrize('way', ['search_list'])
+    @pytest.mark.parametrize('page', [None, 500])
+    def test_case04(self,way,page):  #，转移手机和固话，全部号码,扣除流量额度，不创建外呼计划，
+        Sync_robot_test.case04(way = way,page=page)
+
+    @pytest.mark.parametrize('way', ['search_list'])
+    @pytest.mark.parametrize('page', [None, 500])
+    def test_case05(self, way,page):  # 扣除流量额度，转移手机，全部号码,不创建外呼计划，
+        Sync_robot_test.case05(way=way,page=page)
 
 
 
-if __name__ == '__main__':
-    re = Test_Sync_robot(test_host).test_case04()
-    print(re)
-    # print(re.request.body.decode("unicode_escape"))
-    # print(re.request.url)
-    # print(re.request.headers)
+
